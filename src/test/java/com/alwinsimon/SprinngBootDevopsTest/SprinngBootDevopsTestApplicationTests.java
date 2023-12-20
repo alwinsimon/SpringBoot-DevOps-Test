580a102dff4f83fc56c12d2850f3868e1a962543
Dr. Alwin Simon <003alwin@gmail.com>
2023-12-20 18:34:38 +0530

    Initial commit

---
+++ src/test/java/com/alwinsimon/SprinngBootDevopsTest/SprinngBootDevopsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alwinsimon.SprinngBootDevopsTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinngBootDevopsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
